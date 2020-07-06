a7df0b828f5ba0d07b5349b35ce8e580b4046e1b
tbm5k <tedburg5@gmail.com>
2020-07-06 11:10:48 +0300

    ...

--- src/main/java/com/chama/app/controllers/AuthController.java
+++ src/main/java/com/chama/app/controllers/AuthController.java
@@ -8,7 +8,7 @@ public class AuthController {
 
     @GetMapping("/login")
     public String loginPage(){
-        return "/layouts/blank";
+        return "fragments/authentication/login";
     }
 
 }
