dcca9b48adf7b890736cc32f8fcf5cda435dc802
tbm5k <tedburg5@gmail.com>
2020-07-16 23:32:50 +0300

    chama service

---
+++ src/main/java/com/chama/app/services/ChamaService.java
@@ -0,0 +1,17 @@
+package com.chama.app.services;
+
+import com.chama.app.models.Chama;
+import com.chama.app.repository.ChamaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChamaService {
+
+    @Autowired
+    ChamaRepo chamaRepo;
+
+    public void addNewChama(Chama chama) {
+        chamaRepo.save(chama);
+    }
+}
