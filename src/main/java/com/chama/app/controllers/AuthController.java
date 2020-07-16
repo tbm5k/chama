461480ff38a4ea0af2a7867ce7ac6d163d738623
tbm5k <tedburg5@gmail.com>
2020-07-16 23:35:20 +0300

    boostrap classes for alert messages and code refactor

--- src/main/java/com/chama/app/controllers/AuthController.java
+++ src/main/java/com/chama/app/controllers/AuthController.java
@@ -6,13 +6,8 @@ import com.chama.app.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AuthController {
@@ -36,8 +31,6 @@ public class AuthController {
 
     @PostMapping("/sign-up")
     public String addNewUser(User user, Model model){
-
-        User userExists = userRepo.findByEmail(user.getEmail());
 
         if(userRepo.findByEmail(user.getEmail()) != null){
             model.addAttribute("error","Email exists");
