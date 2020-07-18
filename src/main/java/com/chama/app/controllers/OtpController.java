a249e6260552afa852743fd82026c0a1d7753390
tbm5k <tedburg5@gmail.com>
2020-07-19 01:35:47 +0300

    OTP implementation

--- src/main/java/com/chama/app/controllers/OtpController.java
+++ src/main/java/com/chama/app/controllers/OtpController.java
@@ -5,10 +5,10 @@ import com.chama.app.services.OtpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OtpController {
@@ -19,16 +19,14 @@ public class OtpController {
     @Autowired
     OtpService otpService;
 
-    //An otp is genereated once this method is called and sent to the client via email
+    //An otp is generated once this method is called and sent to the client via email
     @GetMapping("/generateOtp")
-    public String optGeneration(User user){
+    public String otpGeneration(User user){
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        String username = authentication.getName();
-        int otp = otpService.otpGeneration(username);
+        String firstName = user.getFirstName();
+        int otp = otpService.otpGeneration(firstName);
 
         mailMessage.setTo(user.getEmail());
         mailMessage.setText(String.valueOf(otp));
@@ -36,6 +34,26 @@ public class OtpController {
         mailSender.send(mailMessage);
 
         return "fragments/authentication/otp";
+    }
+
+    @PostMapping("/otpValidation")
+    public String otpValidation(int otp, Model model, User user){
+
+        String firstName = user.getFirstName();
+
+        if(otp >= 0){
+            int serverOtp = otpService.getOtp(firstName);
+
+            if(otp > 0){
+                if(otp == serverOtp){
+                    model.addAttribute("valid","Valid");
+                }else {
+                    model.addAttribute("error","Invalid token, try again");
+                }
+            }
+        }
+
+        return "redirect:fragments/chama/chama";
     }
 
 }
--- src/main/java/com/chama/app/services/OtpService.java
+++ src/main/java/com/chama/app/services/OtpService.java
@@ -1,18 +1,34 @@
 package com.chama.app.services;
 
+import com.google.common.cache.LoadingCache;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class OtpService {
 
-    //method to randomly generate a opt
+    LoadingCache<String, Integer> otpCache;
+
+    //This method is used to push the opt number against Key. Rewrite the OTP if it exists
+    //Using user id  as key
     public int otpGeneration(String key) {
 
         Random random = new Random();
-         int otp = 100000 + random.nextInt(900000);
-
+        int otp = 100000 + random.nextInt(900000);
+        otpCache.put(key, otp);
         return otp;
+    }
+
+    //returns the otp against key(first name)
+    public int getOtp(String key) {
+
+        try {
+            otpCache.get(key);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
