698b3c928c2726e2653c5c6bfb890845abcb865e
tbm5k <tedburg5@gmail.com>
2020-07-18 15:57:03 +0300

    guava lib and mail dependency

--- src/main/java/com/chama/app/controllers/OtpController.java
+++ src/main/java/com/chama/app/controllers/OtpController.java
@@ -1,7 +1,10 @@
 package com.chama.app.controllers;
 
+import com.chama.app.models.User;
 import com.chama.app.services.OtpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -11,16 +14,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class OtpController {
 
     @Autowired
+    JavaMailSender mailSender;
+
+    @Autowired
     OtpService otpService;
 
-    //An otp is genereated once this method is called
+    //An otp is genereated once this method is called and sent to the client via email
     @GetMapping("/generateOtp")
-    public String optGeneration(){
+    public String optGeneration(User user){
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         String username = authentication.getName();
         int otp = otpService.otpGeneration(username);
+
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setText(String.valueOf(otp));
+
+        mailSender.send(mailMessage);
 
         return "fragments/authentication/otp";
     }
