bc6d7d78408a0d7b55c6c5791cbd921b1c9a7f51
tbm5k <tedburg5@gmail.com>
2020-07-22 12:53:05 +0300

    Adding opt to the auth controller

--- src/main/java/com/chama/app/controllers/AuthController.java
+++ src/main/java/com/chama/app/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.chama.app.controllers;
 
 import com.chama.app.repository.UserRepo;
+import com.chama.app.services.MailService;
+import com.chama.app.services.OtpService;
 import com.chama.app.services.UserService;
 import com.chama.app.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
@@ -17,6 +20,16 @@ public class AuthController {
 
     @Autowired
     UserRepo userRepo;
+
+    @Autowired
+    OtpService otpService;
+
+    @Autowired
+    MailService mailService;
+
+    //Global variables
+    public String username;
+    public String email;
 
     @GetMapping("/sign-in")
     public String loginPage(){
@@ -32,6 +45,9 @@ public class AuthController {
     @PostMapping("/sign-up")
     public String addNewUser(User user, Model model){
 
+        username = user.getUsername();
+        email = user.getEmail();
+
         if(userRepo.findByUsername(user.getUsername()) != null){
             model.addAttribute("usernameError", "Username is taken");
             return "fragments/authentication/sign-up";
@@ -42,8 +58,37 @@ public class AuthController {
             return "fragments/authentication/sign-up";
         } else{
             userService.addNewUser(user);
-            return "redirect:generateOtp";
+            otpGeneration(username, email);
+            return "fragments/authentication/otp";
         }
+    }
+
+    @GetMapping("/otpValidation")
+    public String otpValidation(@RequestParam("otp") Integer otp, Model model){
+
+        if(otp >= 0){
+            int serverOtp = otpService.getOtp(username);
+
+            if(otp > 0){
+                if(otp == serverOtp){
+                    otpService.clearOtp(username);
+                    model.addAttribute("valid","Valid");
+                    return "redirect:chama";
+                }else {
+                    model.addAttribute("error","Invalid token, try again");
+                }
+            }
+        }
+
+        return "fragments/authentication/otp";
+    }
+
+    public void otpGeneration(String username, String email){
+
+        int opt = otpService.otpGeneration(username);
+
+        mailService.sendMail(email, String.valueOf(opt));
+
     }
 
     @GetMapping("/resetPassword")
