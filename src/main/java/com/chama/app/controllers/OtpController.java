fab918837d3d45e5369c0be45a2a17693a360492
tbm5k <tedburg5@gmail.com>
2020-07-22 12:53:49 +0300

    commenting the whole class

--- src/main/java/com/chama/app/controllers/OtpController.java
+++ src/main/java/com/chama/app/controllers/OtpController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/*
 @Controller
 public class OtpController {
 
@@ -49,3 +50,4 @@ public class OtpController {
     }
 
 }
+*/
