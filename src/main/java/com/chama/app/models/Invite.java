436370a05a3d9a903d35b4cfa212974f0dc089af
tbm5k <tedburg5@gmail.com>
2020-07-29 21:41:19 +0300

    invite entity

---
+++ src/main/java/com/chama/app/models/Invite.java
@@ -0,0 +1,56 @@
+package com.chama.app.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Invite {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "uuid")
+    private String uuid;
+    @Column(name = "us_id_fk")
+    private int userForeignKey;
+    @Column(name = "ch_id_fk")
+    private int chamaForeignKey;
+
+    public Invite() {
+        this.uuid = String.valueOf(UUID.randomUUID());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public int getUserForeignKey() {
+        return userForeignKey;
+    }
+
+    public void setUserForeignKey(int userForeignKey) {
+        this.userForeignKey = userForeignKey;
+    }
+
+    public int getChamaForeignKey() {
+        return chamaForeignKey;
+    }
+
+    public void setChamaForeignKey(int chamaForeignKey) {
+        this.chamaForeignKey = chamaForeignKey;
+    }
+}
