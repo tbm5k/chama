9daeb67074bac3e31c296974e780a6d4a1a76b6b
tbm5k <tedburg5@gmail.com>
2020-08-05 14:12:50 +0300

    more fields

--- src/main/java/com/chama/app/models/Receipt.java
+++ src/main/java/com/chama/app/models/Receipt.java
@@ -13,8 +13,18 @@ public class Receipt {
     private int id;
     @Column(name = "uuid")
     private String uuid;
-    @Column(name = "receipt_no")
+    @Column(name = "re_number")
     private int receiptNumber;
+    @Column(name = "re_amount")
+    private int amount;
+    @Column(name = "re_payment_mode")
+    private String paymentMode;
+    @Column(name = "re_payment_description")
+    private String paymentDescription;
+    @Column(name = "re_status")
+    private boolean status;
+    @Column(name = "re_contribution_type")
+    private String contributionType;
     @Column(name = "us_id_fk")
     private int userForeignKey;
 
@@ -44,6 +54,46 @@ public class Receipt {
 
     public void setReceiptNumber(int receiptNumber) {
         this.receiptNumber = receiptNumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(String paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public String getPaymentDescription() {
+        return paymentDescription;
+    }
+
+    public void setPaymentDescription(String paymentDescription) {
+        this.paymentDescription = paymentDescription;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getContributionType() {
+        return contributionType;
+    }
+
+    public void setContributionType(String contributionType) {
+        this.contributionType = contributionType;
     }
 
     public int getUserForeignKey() {
