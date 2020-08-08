19432e536b049306aca1a71ecb5496388de1225d
tbm5k <tedburg5@gmail.com>
2020-08-08 13:50:35 +0300

    receipt controller

---
+++ src/main/java/com/chama/app/controllers/ReceiptController.java
@@ -0,0 +1,9 @@
+package com.chama.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ReceiptController {
+
+}
