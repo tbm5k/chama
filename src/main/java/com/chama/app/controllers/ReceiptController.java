bd4d62038ed370554ed6361384e64dbc9fe8f194
tbm5k <tedburg5@gmail.com>
2020-08-10 21:07:54 +0300

    receipt page controller

--- src/main/java/com/chama/app/controllers/ReceiptController.java
+++ src/main/java/com/chama/app/controllers/ReceiptController.java
@@ -1,9 +1,15 @@
 package com.chama.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReceiptController {
 
+    @GetMapping("/receipt")
+    public String getReceipt(){
+        return "fragments/receipt/receipt";
+    }
+    
 }
