f1f9442335975c26f0727ab57a941e74b47ff0ce
tbm5k <tedburg5@gmail.com>
2020-08-12 14:01:33 +0300

    updates and new classes

--- src/main/java/com/chama/app/controllers/ReceiptController.java
+++ src/main/java/com/chama/app/controllers/ReceiptController.java
@@ -1,14 +1,21 @@
 package com.chama.app.controllers;
 
+import com.chama.app.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReceiptController {
 
+    @Autowired
+    MemberService memberService;
+
     @GetMapping("/receipt")
-    public String getReceipt(){
+    public String getReceipt(Model model){
+        model.addAttribute("members", memberService.findAllMembers());
         return "fragments/receipt/receipt";
     }
     
---
+++ src/main/java/com/chama/app/models/Member.java
@@ -0,0 +1,58 @@
+package com.chama.app.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "me_member")
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "uuid")
+    private String uuid;
+    @Column(name = "us_id_fk")
+    private int userForeignKey;
+    @Column(name = "ch_id_fk")
+    private int chamaForeignKey;
+
+    public Member() {
+        this.uuid = String.valueOf(UUID.randomUUID());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public int getUserForeignKey() {
+        return userForeignKey;
+    }
+
+    public void setUserForeignKey(int userForeignKey) {
+        this.userForeignKey = userForeignKey;
+    }
+
+    public int getChamaForeignKey() {
+        return chamaForeignKey;
+    }
+
+    public void setChamaForeignKey(int chamaForeignKey) {
+        this.chamaForeignKey = chamaForeignKey;
+    }
+}
