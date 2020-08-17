0eb2c572562b8809fc96ef509a700e07baea794d
tbm5k <tedburg5@gmail.com>
2020-08-17 23:59:22 +0300

    stitching the receipt module

--- src/main/java/com/chama/app/controllers/ReceiptController.java
+++ src/main/java/com/chama/app/controllers/ReceiptController.java
@@ -1,10 +1,12 @@
 package com.chama.app.controllers;
 
+import com.chama.app.models.Receipt;
 import com.chama.app.services.UserIntegrationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReceiptController {
@@ -14,7 +16,15 @@ public class ReceiptController {
 
     @GetMapping("/receipt")
     public String getReceipt(Model model){
-        model.addAttribute("members", userIntegrationsService.findByChamaForeignKey(2));
+        //dynamically set the chama id
+        //model.addAttribute("members", userIntegrationsService.findByChamaForeignKey(2));
+        model.addAttribute("receipt", new Receipt());
+        return "fragments/receipt/receipt";
+    }
+
+    @PostMapping("/receipt")
+    public String setReceipt(){
+        System.out.println("Receipt posted");
         return "fragments/receipt/receipt";
     }
     
