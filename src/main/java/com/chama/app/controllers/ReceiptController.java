bffb7dc786d419fea9677bf01b7b16505b965cc7
tbm5k <tedburg5@gmail.com>
2020-08-17 16:29:45 +0300

    implementing user integrations to display chama users

--- src/main/java/com/chama/app/controllers/ReceiptController.java
+++ src/main/java/com/chama/app/controllers/ReceiptController.java
@@ -1,21 +1,20 @@
 package com.chama.app.controllers;
 
-import com.chama.app.services.MemberService;
+import com.chama.app.services.UserIntegrationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReceiptController {
 
     @Autowired
-    MemberService memberService;
+    UserIntegrationsService userIntegrationsService;
 
     @GetMapping("/receipt")
     public String getReceipt(Model model){
-        model.addAttribute("members", memberService.findAllMembers());
+        model.addAttribute("members", userIntegrationsService.findByChamaForeignKey(2));
         return "fragments/receipt/receipt";
     }
     
