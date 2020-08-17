68c37033928554097ae184fcf7d1df31ec98a5c6
tbm5k <tedburg5@gmail.com>
2020-08-17 16:26:19 +0300

    user integrations repo

---
+++ src/main/java/com/chama/app/repository/UserIntegrationsRepo.java
@@ -0,0 +1,9 @@
+package com.chama.app.repository;
+
+import com.chama.app.models.UserIntegrations;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserIntegrationsRepo extends CrudRepository<UserIntegrations, Integer> {
+
+    Iterable<UserIntegrations> findByChamaForeignKey(int chamaId);
+}
