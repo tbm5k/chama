4d2c4092bc810936e190c28fb502a2648b9f03d9
tbm5k <tedburg5@gmail.com>
2020-11-20 19:28:22 +0300

    validation method that previews uploaded data on a template

--- src/main/java/com/chama/app/controllers/multipledata/ExcelAllocationController.java
+++ src/main/java/com/chama/app/controllers/multipledata/ExcelAllocationController.java
@@ -1,14 +1,18 @@
 package com.chama.app.controllers.multipledata;
 
+import com.chama.app.models.Allocation;
+import com.chama.app.models.multipledata.ExcelAllocation;
 import com.chama.app.services.multipledata.ExcelAllocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class ExcelAllocationController {
@@ -31,9 +35,10 @@ public class ExcelAllocationController {
         return "fragments/allocation/allocation-upload";
     }
 
-    @GetMapping("/va")
-    public String validate(){
-        allocationService.validateRecords(1);
-        return "fragments/allocation/allocation-upload";
+    @GetMapping("/allocationsPreview")
+    public String validate(Model model){
+        List<ExcelAllocation> allocationList = allocationService.getChamaAllocations(1);//dynamically set the chama id
+        model.addAttribute("allocations", allocationList);
+        return "fragments/allocation/allocations-preview";
     }
 }
