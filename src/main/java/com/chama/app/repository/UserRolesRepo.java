9239c9751885c353152496413fa133d7338f22aa
Teddy <tedburg5@gmail.com>
2021-01-05 10:22:33 +0300

    query to return user role object

--- src/main/java/com/chama/app/repository/UserRolesRepo.java
+++ src/main/java/com/chama/app/repository/UserRolesRepo.java
@@ -1,8 +1,13 @@
 package com.chama.app.repository;
 
 import com.chama.app.models.UserRoles;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRolesRepo extends CrudRepository<UserRoles, Integer> {
-    UserRoles findByUserForeignKey(int userForeignKey);
+
+    @Query(nativeQuery = true,
+            value = "select * from ur_user_roles where us_id_fk = :us_id_fk")
+    UserRoles findByUserForeignKey(@Param("us_id_fk") int userForeignKey);
 }
