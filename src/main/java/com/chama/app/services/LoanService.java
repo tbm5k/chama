d08b2d328b71918981d4ed74dee2b066e745d94f
Teddy <tedburg5@gmail.com>
2021-01-16 14:11:42 +0300

    method that returns the chama's loans

--- src/main/java/com/chama/app/services/LoanService.java
+++ src/main/java/com/chama/app/services/LoanService.java
@@ -5,6 +5,9 @@ import com.chama.app.repository.LoanRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class LoanService {
 
@@ -13,5 +16,16 @@ public class LoanService {
 
     public void addLoan(Loan loan) {
         loanRepo.save(loan);
+    }
+
+    public List<Loan> getLoanRequests(int chamaId) {
+        List<Loan> loans = (List<Loan>) loanRepo.findAll();
+        List<Loan> loanList = new ArrayList<>();
+        for(Loan loan : loans){
+            if(loan.getMember().getChama().getChamaId() == chamaId){
+                loanList.add(loan);
+            }
+        }
+        return loanList;
     }
 }
