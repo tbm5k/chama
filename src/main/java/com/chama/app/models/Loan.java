8c927fd09f32f1f8ea8ad03e51d224c31c899803
Teddy <tedburg5@gmail.com>
2021-01-19 15:49:17 +0300

    addition of the loan period property

--- src/main/java/com/chama/app/models/Loan.java
+++ src/main/java/com/chama/app/models/Loan.java
@@ -1,6 +1,7 @@
 package com.chama.app.models;
 
 import javax.persistence.*;
+import java.sql.Date;
 import java.util.UUID;
 
 @Entity
@@ -15,10 +16,16 @@ public class Loan {
     private String uuid;
     @Column(name = "lo_amount")
     private int amount;
+    @Column(name = "lo_period")
+    private Date loanPeriod;
 
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "ui_id_fk")
     private UserIntegrations member;
+
+    public Loan() {
+        this.uuid = String.valueOf(UUID.randomUUID());
+    }
 
     public int getLoanId() {
         return loanId;
@@ -42,6 +49,14 @@ public class Loan {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    public Date getLoanPeriod() {
+        return loanPeriod;
+    }
+
+    public void setLoanPeriod(Date loanPeriod) {
+        this.loanPeriod = loanPeriod;
     }
 
     public UserIntegrations getMember() {
